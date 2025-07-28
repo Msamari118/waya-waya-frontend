d6724227c636efe70b3a00d62ffe272a5245485c
Sandile Lunga <sandilelunga833@gmail.com>
2025-07-28 13:42:05 +0200

    final errors deleted

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './',
-  build: {
-    outDir: 'build',  // change this from 'dist' to 'build'
-    emptyOutDir: true,
-  }
-});
